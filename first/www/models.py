b7a1fe835b94bcdb71775561662f1f3252abf713
Craig Houston <craighouston01@gmail.com>
2017-09-14 23:14:24 +0100

    Styling added to app

--- first/www/models.py
+++ first/www/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Player(models.Model):
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
+    image = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.firstname + " " + self.lastname
@@ -18,7 +19,7 @@ class Match(models.Model):
 class Appearance(models.Model):
     match = models.ForeignKey(Match)
     player = models.ForeignKey(Player)
-    paid = models.BooleanField()
+    paid = models.BooleanField(default=True)
 
     def __str__(self):
         return str(self.match) + " - " + str(self.player)
--- first/www/tests.py
+++ first/www/tests.py
@@ -1,3 +1,14 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
+from .views import index
 
-# Create your tests here.
+class HomeTests(TestCase):
+    def test_index_view_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_index_url_resolves_index_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, index)
