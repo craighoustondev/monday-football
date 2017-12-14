dc293777f3c34c05e4fca69f35255b98177442a4
Craig Houston <craighouston01@gmail.com>
2017-12-14 22:47:54 +0000

    Changes to functional tests

--- first/functional_tests.py
+++ first/functional_tests.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+class IndexTest(unittest.TestCase):
 
-assert 'Monday Night Indoor Football' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_index_page_title(self):
+        self.browser.get('http://127.0.0.1:8000')
+        assert 'Monday Night Indoor Football' in self.browser.title
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
